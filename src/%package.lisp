d3a6259379eb41f24f5ec606387eb030cc754719
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2018-01-23 14:42:26 -0500

    Initial commit

---
+++ src/%package.lisp
@@ -0,0 +1,25 @@
+;;;clution.lib - project development tools for CL
+;;;Written in 2018 by Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
+;;;
+;;;To the extent possible under law, the author(s) have dedicated all copyright
+;;;and related and neighboring rights to this software to the public domain
+;;;worldwide. This software is distributed without any warranty.
+;;;You should have received a copy of the CC0 Public Domain Dedication along
+;;;with this software. If not, see
+;;;<http://creativecommons.org/publicdomain/zero/1.0/>.
+
+(defpackage #:%clution.lib
+  (:use
+   #:alexandria
+   #:cl
+   #:cl-arrows
+   #:enumerable
+   #:parse-float)
+  (:export
+   ;;;util
+   #:%whitespacep
+   #:%terminating-p
+   #:%cell-before
+   #:%cell-after
+   #:%item-before
+   #:%item-after))
